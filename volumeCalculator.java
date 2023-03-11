2510b2fe95aaf2f3eba8786c4b907555a8889ed1
Salil Debnath <salildebnath43@gmail.com>
2023-03-11 23:42:06 +0530

    3D Volume Calculator

--- volumeCalculator.java
+++ volumeCalculator.java
@@ -94,117 +94,118 @@ class test_volumeCalculator {
 
 class volumeCalculator {
 	public static void main(String args[]) {
-		Scanner s = new Scanner(System.in);
-		int op = 0;
-		int c = 0;
-		double Length = 0;
-		double Breadth = 0;
-		double Height = 0;
-		double Radius = 0;
-		do {
-			System.out.println("You want volume of which 3D object :");
-			System.out.println("1-Cube");
-			System.out.println("2-Cuboid");
-			System.out.println("3-Cylinder");
-			op = s.nextInt();
-			switch (op) {
-				case 1: {
-					int again = 0;
-					System.out.println("Enter the Length :");
-					Length = s.nextDouble();
-					test_volumeCalculator cube = new test_volumeCalculator(Length);
+		try (Scanner s = new Scanner(System.in)) {
+            int op = 0;
+            int c = 0;
+            double Length = 0;
+            double Breadth = 0;
+            double Height = 0;
+            double Radius = 0;
+            do {
+            	System.out.println("You want volume of which 3D object :");
+            	System.out.println("1-Cube");
+            	System.out.println("2-Cuboid");
+            	System.out.println("3-Cylinder");
+            	op = s.nextInt();
+            	switch (op) {
+            		case 1: {
+            			int again = 0;
+            			System.out.println("Enter the Length :");
+            			Length = s.nextDouble();
+            			test_volumeCalculator cube = new test_volumeCalculator(Length);
 
-					System.out.println("Volume of Cube:" + cube.CubeVolume());
-					System.out.println("Do you want to change the data and again Calculate the Volume:Y==1/N==0");
-					again = s.nextInt();
-					if (again == 1) {
-						System.out.println("Enter the new Length :");
-						Length = s.nextDouble();
-						cube.setLength(Length);
-						System.out.println("Volume of Cube:" + cube.CubeVolume());
-					}
-					System.out.println("Do you want to continue:(Y==1/N==0)");
-					c = s.nextInt();
-					break;
-				}
-				case 2: {
-					int again = 0;
-					boolean val;
-					System.out.println("Enter the Length:");
-					Length = s.nextDouble();
+            			System.out.println("Volume of Cube:" + cube.CubeVolume());
+            			System.out.println("Do you want to change the data and again Calculate the Volume:Y==1/N==0");
+            			again = s.nextInt();
+            			if (again == 1) {
+            				System.out.println("Enter the new Length :");
+            				Length = s.nextDouble();
+            				cube.setLength(Length);
+            				System.out.println("Volume of Cube:" + cube.CubeVolume());
+            			}
+            			System.out.println("Do you want to continue:(Y==1/N==0)");
+            			c = s.nextInt();
+            			break;
+            		}
+            		case 2: {
+            			int again = 0;
+            			boolean val;
+            			System.out.println("Enter the Length:");
+            			Length = s.nextDouble();
 
-					System.out.println("Enter the Breadth:");
-					Breadth = s.nextDouble();
+            			System.out.println("Enter the Breadth:");
+            			Breadth = s.nextDouble();
 
-					System.out.println("Enter the Height:");
-					Height = s.nextDouble();
+            			System.out.println("Enter the Height:");
+            			Height = s.nextDouble();
 
-					test_volumeCalculator cuboid = new test_volumeCalculator(Length, Breadth, Height);
-					System.out.println("Volume of Cuboid:" + cuboid.CuboidVolume());
+            			test_volumeCalculator cuboid = new test_volumeCalculator(Length, Breadth, Height);
+            			System.out.println("Volume of Cuboid:" + cuboid.CuboidVolume());
 
-					val = cuboid.IsCube();
-					System.out.println("Is your cuboid also a cube:" + val);
+            			val = cuboid.IsCube();
+            			System.out.println("Is your cuboid also a cube:" + val);
 
-					System.out.println("Do you want to change the data and again Calculate the Volume:Y==1/N==0");
-					again = s.nextInt();
-					if (again == 1) {
-						System.out.println("Enter the new Length:");
-						Length = s.nextDouble();
+            			System.out.println("Do you want to change the data and again Calculate the Volume:Y==1/N==0");
+            			again = s.nextInt();
+            			if (again == 1) {
+            				System.out.println("Enter the new Length:");
+            				Length = s.nextDouble();
 
-						System.out.println("Enter the new Breadth:");
-						Breadth = s.nextDouble();
+            				System.out.println("Enter the new Breadth:");
+            				Breadth = s.nextDouble();
 
-						System.out.println("Enter the new Height:");
-						Height = s.nextDouble();
+            				System.out.println("Enter the new Height:");
+            				Height = s.nextDouble();
 
-						cuboid.setLength(Length);
-						cuboid.setBreadth(Breadth);
-						cuboid.setHeight(Height);
+            				cuboid.setLength(Length);
+            				cuboid.setBreadth(Breadth);
+            				cuboid.setHeight(Height);
 
-						System.out.println("Volume of Cuboid:" + cuboid.CuboidVolume());
+            				System.out.println("Volume of Cuboid:" + cuboid.CuboidVolume());
 
-						val = cuboid.IsCube();
-						System.out.println("Is your cuboid also a cube: " + val);
-					}
-					System.out.println("Do you want to continue:(Y==1/N==0)");
-					c = s.nextInt();
-					break;
-				}
-				case 3: {
-					int again = 0;
-					System.out.println("Enter the Radius:");
-					Radius = s.nextDouble();
+            				val = cuboid.IsCube();
+            				System.out.println("Is your cuboid also a cube: " + val);
+            			}
+            			System.out.println("Do you want to continue:(Y==1/N==0)");
+            			c = s.nextInt();
+            			break;
+            		}
+            		case 3: {
+            			int again = 0;
+            			System.out.println("Enter the Radius:");
+            			Radius = s.nextDouble();
 
-					System.out.println("Enter the Height:");
-					Height = s.nextDouble();
-					test_volumeCalculator cylinder = new test_volumeCalculator(Radius, Height);
-					System.out.println("Volume of Cylinder:" + cylinder.CylinderVolume());
+            			System.out.println("Enter the Height:");
+            			Height = s.nextDouble();
+            			test_volumeCalculator cylinder = new test_volumeCalculator(Radius, Height);
+            			System.out.println("Volume of Cylinder:" + cylinder.CylinderVolume());
 
-					System.out.println("Do you want to change the data and again Calculate the Volume:Y==1/N==0");
-					again = s.nextInt();
-					if (again == 1) {
-						System.out.println("Enter the new Radius:");
-						Radius = s.nextDouble();
+            			System.out.println("Do you want to change the data and again Calculate the Volume:Y==1/N==0");
+            			again = s.nextInt();
+            			if (again == 1) {
+            				System.out.println("Enter the new Radius:");
+            				Radius = s.nextDouble();
 
-						System.out.println("Enter the new Height:");
-						Height = s.nextDouble();
+            				System.out.println("Enter the new Height:");
+            				Height = s.nextDouble();
 
-						cylinder.setRadius(Radius);
-						cylinder.setHeight(Height);
+            				cylinder.setRadius(Radius);
+            				cylinder.setHeight(Height);
 
-						System.out.println("Volume of Cylinder: " + cylinder.CylinderVolume());
-					}
-					System.out.println("Do you want to continue:(Y==1/N==0)");
-					c = s.nextInt();
-					break;
-				}
-				default: {
-					System.out.println("Enter Valid Input");
-					System.out.println("Do you want to continue:(Y==1/N==0)");
-					c = s.nextInt();
-					break;
-				}
-			}
-		} while (c == 1);
+            				System.out.println("Volume of Cylinder: " + cylinder.CylinderVolume());
+            			}
+            			System.out.println("Do you want to continue:(Y==1/N==0)");
+            			c = s.nextInt();
+            			break;
+            		}
+            		default: {
+            			System.out.println("Enter Valid Input");
+            			System.out.println("Do you want to continue:(Y==1/N==0)");
+            			c = s.nextInt();
+            			break;
+            		}
+            	}
+            } while (c == 1);
+        }
 	}
 }
